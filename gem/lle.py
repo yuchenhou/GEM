4ec0d3df64a46649b2d6624fa920a65b3eed1251
Alan Hou <houych@gmail.com>
2017-08-28 21:26:33 -0700

    reformat the code

--- gem/lle.py
+++ gem/lle.py
@@ -5,18 +5,19 @@ import pandas
 
 def main(data_set_name):
     dimensions = 4
-    input_file = './graph/'+ data_set_name + '.tsv'
+    input_file = './graph/' + data_set_name + '.tsv'
     output_file = './emb/' + data_set_name + '.emb'
     # Instatiate the embedding method with hyperparameters
-    graph_factorization = LocallyLinearEmbedding(dimensions)
+    lle = LocallyLinearEmbedding(dimensions)
 
     # Load graph
     graph = graph_util.loadGraphFromEdgeListTxt(input_file)
 
     # Learn embedding - accepts a networkx graph or file with edge list
-    embeddings_array, t = graph_factorization.learn_embedding(graph, edge_f=None, is_weighted=True, no_python=True)
+    embeddings_array, t = lle.learn_embedding(graph, edge_f=None, is_weighted=True, no_python=True)
     embeddings = pandas.DataFrame(embeddings_array)
     embeddings.to_csv(output_file, sep=' ', na_rep=0.1)
+
 
 if __name__ == '__main__':
     for data_set_name in ['airport', 'collaboration', 'congress', 'forum']:
