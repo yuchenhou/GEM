03e59a703b91f87343d3ebf97020c2a6c881e429
Alan Hou <houych@gmail.com>
2017-08-28 19:47:08 -0700

    evaluate LLE

---
+++ gem/GraphFactorization.py
@@ -0,0 +1,24 @@
+from gem.embedding.gf import GraphFactorization
+from gem.utils import graph_util
+import pandas
+
+
+def main(data_set_name):
+    dimensions = 4
+    input_file = './graph/'+ data_set_name + '.tsv'
+    output_file = './emb/' + data_set_name + '.emb'
+    # Instatiate the embedding method with hyperparameters
+    graph_factorization = GraphFactorization(dimensions, 1000, 1 * 10 ** -4, 1.0)
+
+    # Load graph
+    graph = graph_util.loadGraphFromEdgeListTxt(input_file)
+
+    # Learn embedding - accepts a networkx graph or file with edge list
+    embeddings_array, t = graph_factorization.learn_embedding(graph, edge_f=None, is_weighted=True, no_python=True)
+
+    embeddings = pandas.DataFrame(embeddings_array)
+    embeddings.to_csv(output_file, sep=' ', na_rep=0.1)
+
+if __name__ == '__main__':
+    for data_set_name in ['airport', 'collaboration', 'congress', 'forum']:
+        main(data_set_name)
